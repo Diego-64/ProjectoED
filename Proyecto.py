d792a873bc48645f077ff2b48af1bd0f2859084f
Cristina <cristinabenitezmendez@gmail.com>
2025-04-10 11:11:07 +0200

    Se ha adjuntado el archivo modificado. Se le ha agregado la función impClasificacion()

---
+++ Proyecto.py
@@ -0,0 +1,39 @@
+#------------------------------VARIABLES---------------------------#
+import csv
+liga=[]
+equipos=[]
+#------------------------------FUNCIONES---------------------------#
+#Las funciones correspondientes a la segunda parte deben de ir encima,
+#al igual que las de la tercera parte, que iran encima de las de la
+#segunda parte
+
+#función leerPartidos() 
+def leerPartidos():
+    partidos=[]
+    partido={}
+    try:
+        with open ("liga.csv") as archivo:
+            lector= csv.reader(archivo)
+            next(lector)
+            for row in lector:
+                partido={}
+                partido["Fecha"]=row[0]
+                partido["Equipo 1"]=row[1]
+                partido["Equipo 2"]=row[2]
+                partido["FT"]=row[3]
+                partido["HT"]=row[4]
+                partidos.append(partido)
+    except FileNotFoundError:
+        print("Ha ocurrido un error al intentar abrir el archivo")
+    return partidos
+
+#funcion impClasificacion()
+def impClasificacion(liga):
+    #Función para la segunda parte 
+    equipos=equipos(liga)
+    infoEquipos(datosLiga,equipos)
+
+
+#------------------------------MAIN-------------------------------#    
+liga=leerPartidos()
+impClasificacion(liga)
